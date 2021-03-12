e3cd1612f621232e831c73c7d817a5b55ef051ae
abhijeet <abhijeetranmale@gmail.com>
2021-03-12 20:10:24 +0530

    added changes

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,5 @@
 import React from "react"
-import { BrowserRouter, Switch, Route } from "react-router-dom"
-
+import { HashRouter, Switch, Route } from "react-router-dom"
 import Homepage from "./views/Homepage"
 import About from "./views/About"
 import Skills from "./views/Skills"
@@ -9,7 +8,7 @@ import Portfolio from "./views/Portfolio"
 
 const Routes = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Switch>
                 <Route exact path="/" component={Homepage} />
                 <Route exact path="/about" component={About} />
@@ -17,7 +16,7 @@ const Routes = () => {
                 <Route exact path="/resume" component={Resume} />
                 <Route exact path="/portfolio" component={Portfolio} />
             </Switch>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
