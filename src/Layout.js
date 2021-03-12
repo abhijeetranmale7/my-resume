61c41cc78e8caecdaaf11338833cf7c440b9ebec
abhijeet <abhijeetranmale@gmail.com>
2021-03-12 20:02:34 +0530

    added changes

--- src/Layout.js
+++ src/Layout.js
@@ -13,7 +13,7 @@ const Layout = ({ children }) => {
                 }
             </div>
             <Footer />
-            <a href="#" className="back-to-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short"></i></a>
+            <a className="back-to-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short"></i></a>
         </div>
     )
 }
--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { HashRouter, Switch, Route } from "react-router-dom"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+
 import Homepage from "./views/Homepage"
 import About from "./views/About"
 import Skills from "./views/Skills"
@@ -8,7 +9,7 @@ import Portfolio from "./views/Portfolio"
 
 const Routes = () => {
     return (
-        <HashRouter>
+        <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={Homepage} />
                 <Route exact path="/about" component={About} />
@@ -16,7 +17,7 @@ const Routes = () => {
                 <Route exact path="/resume" component={Resume} />
                 <Route exact path="/portfolio" component={Portfolio} />
             </Switch>
-        </HashRouter>
+        </BrowserRouter>
     )
 }
 
