cdb28cf53274b376797b47c230fa2491b6028cec
abhijeet <abhijeetranmale@gmail.com>
2021-03-12 20:39:12 +0530

    added changes

--- src/Routes.js
+++ src/Routes.js
@@ -9,7 +9,7 @@ import Portfolio from "./views/Portfolio"
 
 const Routes = () => {
     return (
-        <BrowserRouter basename={process.env.REACT_APP_ROUTER_BASE || ''}>
+        <BrowserRouter basename="/">
             <Switch>
                 <Route exact path="/" component={Homepage} />
                 <Route exact path="/about" component={About} />
